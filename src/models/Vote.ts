c9e8ae2b7fbb6f10fad628b4a45084c87205744a
Facto <33882218+Sniped@users.noreply.github.com>
2020-06-25 22:52:48 -0400

    settle vote within schema

--- src/models/Vote.ts
+++ src/models/Vote.ts
@@ -1,4 +1,6 @@
 import { prop, getModelForClass } from "@typegoose/typegoose";
+import { channels } from "../constants/channels";
+import BotClient from "../client/BotClient";
 
 class Vote {
     @prop({ required: true })
@@ -12,6 +14,15 @@ class Vote {
 
     @prop({ required: true })
     public dateExpired!: Date;
+
+    public async settleVote(client: BotClient) {
+        const vote = await VoteModel.findOne({ discordMessage: this.discordMessage }).exec();
+        const voteChannel = await channels.whitelistChannel(client);
+        const voteMessage = await voteChannel.messages.fetch(this.discordMessage);
+        await voteMessage.delete();
+        await vote?.remove();
+    }
+
 }
 
 const VoteModel = getModelForClass(Vote);
