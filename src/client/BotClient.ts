12537cb800588e140dd903d1d8857f878e189d02
Facto <33882218+Sniped@users.noreply.github.com>
2020-06-26 01:08:26 -0400

    default prompts

--- src/client/BotClient.ts
+++ src/client/BotClient.ts
@@ -27,7 +27,16 @@ export default class BotClient extends AkairoClient {
 
         this.commandHandler = new CommandHandler(this, {
             directory: join(__dirname, '..', 'commands'),
-            prefix: '!'
+            prefix: '!',
+            argumentDefaults: {
+                prompt: {
+                    timeout: ':x: Time ran out, command has been cancelled.',
+                    ended: ':x: Too many retries, command has been cancelled.',
+                    cancel: ':x: Command has been cancelled.',
+                    retries: 4,
+                    time: 30000
+                }
+            }
         });
 
         this.eventListener = new ListenerHandler(this, {
@@ -37,7 +46,9 @@ export default class BotClient extends AkairoClient {
         this.commandHandler.useListenerHandler(this.eventListener);
 
         this.commandHandler.resolver.addType('minecraftAccount', async (msg, phrase) => {
+            if (!phrase) return null;
             const account = await MinecraftAccount.fetchByName(phrase);
+            console.log(account);
             if (!account) return null;
             return account;
         });
